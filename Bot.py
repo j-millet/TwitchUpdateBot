eed58a9bd964075ef040dc65f714eefc1e92d584
VegTabs <keszix@gmail.com>
2021-11-27 16:05:39 +0100

    added oauth read

--- Bot.py
+++ Bot.py
@@ -4,6 +4,9 @@ import requests
 import json
 import os
 import asyncio
+import time
+
+from requests.api import get
 
 
 bot = commands.Bot(command_prefix="*")
@@ -13,6 +16,24 @@ with open('auth.json') as f:
     d = json.loads(f.read())
     token,client_id,client_secret = [d[x] for x in d]
 
+async def get_oauth(cid,cs)->str:
+    with open("oauth.json","r") as f:  #checks if we already have a key that is more than a day from expiry
+        d = json.loads(f.read())
+        if d['expiration_timestamp'] - time.time() > 24*3600: return f'Bearer {d["oauth"]}'
+        
+    with open("oauth.json","w") as f: #generate new auth key
+        d = {}
+        data={
+            "client_id":cid,
+            "client_secret":cs,
+            "grant_type":"client_credentials"
+        }
+        r = requests.post("https://id.twitch.tv/oauth2/token",data=data)
+        d['expiration_timestamp'] = (time.time()+r.json()['expires_in'])
+        d['oauth'] = r.json()["access_token"]
+        f.write(json.dumps(d))
+        return f'Bearer {d["oauth"]}'
+
 @bot.command(name="help")
 async def read_commands(ctx,*ver):
     o = "help_extra.txt" if (len(ver)>0 and ver[0]=="adv") else "help.txt"
@@ -20,5 +41,5 @@ async def read_commands(ctx,*ver):
         await ctx.channel.send(f.read())
 
 
-        
-bot.run(token)   
+print(get_oauth(client_id,client_secret))
+#bot.run(token)   
