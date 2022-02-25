eba5f844d82de180634064125ccda9ca226b63f0
VegTabs <keszix@gmail.com>
2022-02-25 12:06:29 +0100

    Looking at this after 6 months I dunno whats going on
    :/

--- Bot.py
+++ Bot.py
@@ -1,7 +1,5 @@
-import discord
 from discord.ext import commands
 import requests
-import json
 import os
 import asyncio
 from Authentication import Authentication
--- GuildFileManager.py
+++ GuildFileManager.py
@@ -3,7 +3,7 @@ import json
 import discord
 import Authentication
 
-def write_sub_file(guild:discord.guild,data:dict)->None:
+def write_sub_file(guild:discord.guild,data:dict)->None:#writes data to guild file, creates file if there isn't one
     p = Authentication.path(f"servers\{guild.id}subs.json")
     with open("servers.json","r+") as f:
         arr = json.loads(f.read())
@@ -16,12 +16,13 @@ def write_sub_file(guild:discord.guild,data:dict)->None:
         f.write(json.dumps(data))
 
 
-def delete_sub_file(guild:discord.guild)->None:
+def delete_sub_file(guild:discord.guild)->bool: #removes guild file
     p = Authentication.path(f"servers\{guild.id}subs.json")
-    if not os.path.exists(p): return
+    if not os.path.exists(p): return False
     os.remove(p)
+    return True
 
-def access_sub_file(guild:discord.guild)->dict:  #returns (guild-file data, guild-file path) tuple, creates file if there isn't one
+def access_sub_file(guild:discord.guild)->dict:  #returns guild-file data, creates file if there isn't one
     p = Authentication.path(f"servers\{guild.id}subs.json")
     data = {}
     if not os.path.exists(p):
